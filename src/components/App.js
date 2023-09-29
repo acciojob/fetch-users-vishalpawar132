215a1687deebb2dc27b0ce6b84f281e23f9f1c0d
vishal pawar <pawarvishal29999@gmail.com>
2023-09-29 12:50:11 +0530

    commit-3

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,69 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-import Userlist from "./Userlist";
+
+
+
+
+
+
 
 const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUserList = () => {
+      setLoading(true);
+  
+      fetch("https://reqres.in/api/users")
+        .then((response) => response.json())
+        .then((data) => {
+          setUsers(data.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          setLoading(false);
+        });
+    };
+
+
+
   return (
     <div>
-        <Userlist/>
+        <button className="btn" onClick={fetchUserList}>Get User Lis</button>
+
+        {loading ? (
+      <p>Loading...</p>
+    ) : (
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.email}</td>
+              <td>
+                <img src={user.avatar} alt="Avatar" width="50" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )}
+
+
     </div>
   )
 }
